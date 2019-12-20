cfa8f1df72d5f9a32f4702c88ac21651d7fc39b0
ALEONDEV <53395716+Leonxv@users.noreply.github.com>
2019-12-20 18:04:19 +0100

    Update Main.php

--- src/Aleondev/Main.php
+++ src/Aleondev/Main.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Aleodnev;
+namespace Aleondev;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\command\CommandSender;
@@ -11,17 +11,16 @@ use onebone\economyapi\EconomyAPI;
 class Main extends PluginBase{
 
 	public function onEnable() : void{
-		$this->getLogger()->info("Aktiviert");
+		$this->getLogger()->info("moneydrop by Aleondev");
 	}
 
 	public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
 		switch($command->getName()){
-			case "payall":
+			case "moneydrop":
 				$zahl = $args[0];
 				foreach ($this->getServer()->getOnlinePlayers() as $player) {
 					$this->getServer()->getPluginManager()->getPlugin("EconomyAPI")->addMoney($player, $zahl);
-					$player->sendMessage("§aDu hast §e" .$zahl. "$ §aerhalten durch einen Moneydrop");
-					EconmyAPI::getInstance()->reduceMoney($player, .$zahl.); 
+					$player->sendMessage("§4Du hast gerade duch rin moneydrop §e" .$zahl. "$§ 4erhalten"); 
 				}
 				return true;
 			default:
