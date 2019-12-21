0102e067a364bd11e44709592668079285181e96
ALEONDEV <53395716+Leonxv@users.noreply.github.com>
2019-12-21 18:44:22 +0100

    Update Main.php

--- src/Aleondev/Main.php
+++ src/Aleondev/Main.php
@@ -16,12 +16,11 @@ class Main extends PluginBase{
 
 	public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
 		switch($command->getName()){
-			case "payall":
+			case "moneydrop":
 				$zahl = $args[0];
 				foreach ($this->getServer()->getOnlinePlayers() as $player) {
 					$this->getServer()->getPluginManager()->getPlugin("EconomyAPI")->addMoney($player, $zahl);
-					$player->sendMessage("§aDu hast §e" .$zahl. "$ §aerhalten durch einen Moneydrop");
-					EconmyAPI::getInstance()->reduceMoney($player, .$zahl.); 
+					$player->sendMessage("§4Du hast §e" .$zahl. "$ §4erhalten durch einen Moneydrop");
 				}
 				return true;
 			default:
